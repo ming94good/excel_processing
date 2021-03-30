9d784b78df795f6c3b57831895654104025bff67
ming94good <harry21105@gmail.com>
2021-03-30 11:27:11 +0800

    update

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ import requests
 import pandas as pd
 import json
 import sqlite3
-import xlwt 
-from xlwt import Workbook
+from openpyxl import Workbook
 import pymongo
+
+sqlDB='new_info.db'
+testCSV='test_log.csv'
+
+# Mongo DB operation
 def insert(user_id, recommend):
     if(count(user_id) > 0):
         for r in recommend:
@@ -27,8 +31,12 @@ def recommend(user_id):
 def delete(user_id):
     query = { "user_id": user_id}
     return col.delete_many(query)
+def clearData(col):
+    col.remove()
+
+# Fetch info from SQL DB(Train result)
 def vtv(video_id):
-    conn = sqlite3.connect('model_info.db')
+    conn = sqlite3.connect(sqlDB)
     sql_cmd = f"SELECT recommended_videos FROM recommended_videos where video_id={video_id}"
     cur = conn.cursor()
     cur.execute(sql_cmd)
@@ -37,30 +45,37 @@ def vtv(video_id):
     conn.close()
     return videos
 
-df = pd.read_csv("test_watch_log.csv")
+# Read test data
+df = pd.read_csv(testCSV)
 
+# MongoDB setup
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 col = mydb["recommend"]
+clearData(col)
 
+# Count True
 c=0
+# Count False
 d=0
 
+# Excel setup
 wb = Workbook()
-sheet = wb.add_sheet('video_recommend') 
-sheet.write(0,0,'user_id')
-sheet.write(0,1,'video_id')
-sheet.write(0,2,'video is in recommend list')
-sheet.write(0,3,'True')
-sheet.write(0,4,'False')
-sheet.write(0,5,'Accuracy')
+sheet = wb.create_sheet(title="video_recommend")
+sheet.cell(row = 1, column = 1).value = 'user_id'
+sheet.cell(row = 1, column = 2).value = 'video_id'
+sheet.cell(row = 1, column = 3).value = 'video is in recommend list'
+sheet.cell(row = 1, column = 4).value = 'True'
+sheet.cell(row = 1, column = 5).value = 'False'
+sheet.cell(row = 1, column = 6).value = 'Accuracy'
+
 for index, row in df.iterrows():
     try:
         recommend_num = 50
         rmd = vtv(str(row['video_id']))
         id = int(row['user_id'])
 
-        recommend_list = recommend(id)
+        recommend_list = recommend(id) #Get recommend videos for the user
         rst = '#'
         if recommend_list:
             if row['video_id'] in recommend_list:
@@ -69,22 +84,20 @@ for index, row in df.iterrows():
             else:
                 d+=1
                 rst = 'False'
-        insert(id,rmd)
+        insert(id,rmd) #Update recommend videos for the user
         
-        sheet.write(index+1,0,str(row['user_id']))
-        sheet.write(index+1,1,str(row['video_id']))
-        sheet.write(index+1,2,str(rst))
+        sheet.cell(index+2, 1).value = str(row['user_id'])
+        sheet.cell(index+2, 2).value = str(row['video_id'])
+        sheet.cell(index+2, 3).value = str(rst)
     except Exception as e:
         print(e)
         continue
-
-    print(index)
     
-sheet.write(1,4,str(c))
-sheet.write(1,5,str(d))
-sheet.write(1,6,str(c/ (c+d)))
-wb.save('result.xls')
+sheet.cell(2, 4).value = str(c)
+sheet.cell(2, 5).value = str(d)
+sheet.cell(2, 6).value = str(c/ (c+d))
+wb.save('result.xlsx')
 print(c)
 print(d)
 print(index)
-print(c/ (c+d))
+print(c/ (c+d)) #Final accuracy
